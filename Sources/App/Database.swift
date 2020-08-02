7815344d684fdcbe73e3ccc9ef021531de52c0e7
Alfred Thekkan <alfred@eatapp.co>
2020-08-02 10:52:54 +0400

    changed db url

--- Sources/App/Database.swift
+++ Sources/App/Database.swift
@@ -25,11 +25,11 @@ extension Environment {
             database = "forty"
             password = "May@2014"
         default:
-            hostName = "localhost"
-            port = 5433
-            username = "postgres"
-            database = "forty"
-            password = "May@2014"
+            hostName = "ec2-54-175-117-212.compute-1.amazonaws.com"
+            port = 5432
+            username = "klcwteqlqrmdag"
+            database = "d10rit9bs0maq0"
+            password = "1e02c6575b1183e33023f8a939600c6d4297ee4a3395ebb4aee8ea3c32ef6edd"
         }
         
         return PostgreSQLDatabaseConfig(hostname: hostName, port: port, username: username, database: database, password: password)
